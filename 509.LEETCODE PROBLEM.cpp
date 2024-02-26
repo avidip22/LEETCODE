3d71638483cd1bac39d884af029d18d4f63c1862
AVISEK MANDAL <136554833+avidip22@users.noreply.github.com>
2024-02-26 22:19:05 +0530

    509.LEETCODE PROBLEM.cpp

--- 509.LEETCODE PROBLEM.cpp
+++ 509.LEETCODE PROBLEM.cpp
@@ -51,3 +51,28 @@ int main()
     
  return 0;
 }
+
+// same problem using Bottom-up approach
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    // step 1 make a 1d array
+    vector<int>dp[n+1];
+    // step-2 
+    dp[0] = 0;
+    dp[1] = 1;
+
+    // step 3 
+    for (int i = 2; i < n; i++)
+    
+        dp[i] = dp[i-1] + dp[i-2];
+      cout<<dp[n]<<endl;
+    
+    
+ return 0;
+}
