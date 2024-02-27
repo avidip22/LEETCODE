1324626a5f7f26af40ec57a4986653fe9aa72244
AVISEK MANDAL <136554833+avidip22@users.noreply.github.com>
2024-02-27 07:44:53 +0530

    746. LEETCODE PROBLEM.cpp

--- 746. LEETCODE PROBLEM.cpp
+++ 746. LEETCODE PROBLEM.cpp
@@ -1,5 +1,7 @@
 // 746. Min Cost Climbing Stairs IN LEETCODE
 
+// FIRST APPROACH IN DP IS TOP DOWN APPROACH 
+
 class Solution {
 public:
 int solve(vector<int>& cost, int n, vector<int>& dp){
@@ -30,3 +32,76 @@ int solve(vector<int>& cost, int n, vector<int>& dp){
          
     }
 };
+// NOW SECOND APPROACH BOTTOM-UP
+// THIS IS TAULATION APPROACH
+class Solution
+{
+public:
+    int solve(vector<int> &cost, int n)
+    {
+        // step 1 creation of dp array
+        vector<int> dp(n + 1);
+        // step 2 base case analysis
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        // step 3
+        for (int i = 2; i < n; i++)
+        {
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+        }
+        return min(dp[n - 1], dp[n - 2]);
+    }
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+
+        int n = cost.size(0);
+        return solve(cost,n);
+    }
+};
+// 3RD APPROACH - SPACE OPTIMIZATION 
+class Solution
+{
+public:
+    int solve(vector<int> &cost, int n)
+    {
+        /
+        // step 2 base case analysis
+       int prev2 = cost[0];
+        int prev2 = cost[1];
+        // step 3
+        for (int i = 2; i < n; i++)
+        {
+            int current = cost[i] + min(prev1,prev2);
+            prev2 = prev1;
+            prev1 = current; 
+        }
+        return min(prev1,prev2);
+    }
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+
+        int n = cost.size(0);
+        return solve(cost,n);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
